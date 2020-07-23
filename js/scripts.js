6be0702f77318015b992cdfc05ff50e11355bc02
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-07-23 12:14:05 -0600

    Revert "Merge branch 'master' of https://github.com/teresasanders77/portfolio-pokedex-2"
    This reverts commit b4e766ae88fafb27223422c538eab88e55c15446, reversing
    changes made to 340560f4e7fc511bf9c2bd81c262ded66602b3ce.

--- js/scripts.js
+++ js/scripts.js
@@ -81,70 +81,6 @@ var pokemonRepository = (function () {
       });
   }
 
-//function to create a list
-function addListItem(pokemon) {
-  var pokemonList = $('.pokemon-list');
-  var $listItem = $('<li></li>');
-  //adds button for each list item
-  var $button = $('<button type="button" class="btn btn-light btn-lg btn-block" data-target="#exampleModal" data-toggle="modal">' + pokemon.name + "</button>");
-  pokemonList.append($listItem);
-  $listItem.append($button);
-  //logs details of each pokemon when clicked
-  $button.on('click', function(event) {
-    showDetails(pokemon);
-  });
-}
-
-function showDetails(pokemon) {
-  pokemonRepository
-  .loadDetails(pokemon).then(function() {
-    console.log(pokemon);
-    showModal(pokemon);
-  });
-}
-
-//function to load list from api
-function loadList() {
-  return $.ajax(apiUrl)
-  .then(function (json) {
-    json.results.forEach(function(item) {
-      var pokemon = {
-        name: item.name,
-        detailsUrl: item.url
-      };
-      add(pokemon);
-    });
-  })
-  //if promise is rejected, the following is run
-  .catch(function(e) {
-    console.error(e);
-  });
-}
-
-//function to load details
-function loadDetails(item) {
-  var url = item.detailsUrl;
-  return $.ajax(url)
-  .then(function (details) {
-    item.imageUrl = details.sprites.front_default;
-    item.height = details.height;
-    item.weight = details.weight;
-    //loop for each type of pokemon
-    item.types = [];
-    for (var i = 0; i < details.types.length; i++) {
-      item.types.push(details.types[i].type.name);
-    }
-//loop for the abilities of each pokemon
-    item.abilities = [];
-    for (var i = 0; i < details.abilities.length; i++) {
-      item.abilities.push(details.abilities[i].ability.name);
-    }
-  })
-    .catch(function(e) {
-      console.error(e);
-  });
-}
-
   function showModal(pokemon) {
     var modalBody = $('.modal-body');
     var modalTitle = $('.modal-title');
@@ -172,26 +108,6 @@ function loadDetails(item) {
     imageElement.attr('src', pokemon.imageUrl);
     modalBody.append(imageElement);
   }
-
-  var nameElement = $('<h1>' + pokemon.name + '</h1>')
-  modalTitle.append(nameElement);
-
-  var heightElement = $('<p>' + 'Height: ' + pokemon.height + '</p>');
-  modalBody.append(heightElement);
-
-  var weightElement = $('<p>' + 'Weight : ' + pokemon.weight + '</p>');
-  modalBody.append(weightElement);
-
-  var typesElement = $('<p>' + 'Types: ' + pokemon.types + '</p>');
-  modalBody.append(typesElement);
-
-  var abilitiesElement = $('<p>' + 'Abilities: ' + pokemon.abilities + '</p>');
-  modalBody.append(abilitiesElement);
-
-  var imageElement = $('<img class="modal-img">');
-  imageElement.attr('src', pokemon.imageUrl);
-  modalBody.append(imageElement);
-}
 
   //returns values that can be accessed to outside the IIFE
   return {
